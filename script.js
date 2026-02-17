a98e0d01b78f856ea3706d952d7eb6c187849670
alinamollishteanu-coder <alina.mollishteanu@gmail.com>
2026-02-17 21:04:17 +0200

    Add files via upload
    Добавлены файлы лабораторной работы

---
+++ script.js
@@ -0,0 +1,17 @@
+let name = "Alina";
+birthYear = 2006;
+isStudent = true;
+alert("Этот код выполнен из внешнего файла!");
+console.log(name, birthYear, isStudent);
+let score = prompt("Введите ваш балл:");
+if (score >= 90) {
+ console.log("Отлично!");
+} else if (score >= 70) {
+ console.log("Хорошо");
+} else {
+ console.log("Можно лучше!");
+}
+
+for (let i = 1; i <= 5; i++) {
+ console.log(`Итерация: ${i}`);
+}
